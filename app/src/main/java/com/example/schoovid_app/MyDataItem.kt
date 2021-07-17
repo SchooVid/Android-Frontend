4f289f086a2db8da619af7ab15fe609d0b2b48ba
David NGUYEN <nguyen.david.1909@gmail.com>
2021-07-18 00:36:03 +0200

    Add file data for course

---
+++ app/src/main/java/com/example/schoovid_app/MyDataItem.kt
@@ -0,0 +1,24 @@
+package com.example.callapi
+
+import com.google.gson.annotations.SerializedName
+
+data class MyDataItem(
+    @SerializedName("categorieId")
+    val categorieId: String,
+    @SerializedName("date_diffusion")
+    val dateDiffusion: String,
+    @SerializedName("date_fin_diffusion")
+    val dateFinDiffusion: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("formateurId")
+    val formateurId: String,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("libelle")
+    val libelle: String,
+    @SerializedName("lien_diffusion")
+    val lienDiffusion: Any,
+    @SerializedName("niveauId")
+    val niveauId: String
+)
