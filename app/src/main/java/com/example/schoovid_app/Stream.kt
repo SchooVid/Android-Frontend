7e69e38a4affd003fc6c57e240803ef636b39ae8
David NGUYEN <nguyen.david.1909@gmail.com>
2021-07-28 00:37:45 +0200

    Add chat with Socket

--- app/src/main/java/com/example/schoovid_app/Stream.kt
+++ app/src/main/java/com/example/schoovid_app/Stream.kt
@@ -2,13 +2,10 @@ package com.example.schoovid_app
 
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.DefaultRenderersFactory
@@ -19,72 +16,18 @@ import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView
-import kotlinx.android.synthetic.main.stream_chat.*
+import kotlinx.android.synthetic.main.stream.*
 
-//class StreamChat : AppCompatActivity(){
-class StreamChat : Fragment(){
+class Stream : Fragment() {
 
-    //private var player: SimpleExoPlayer? = null
-
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.stream_chat)
-
-        play()
-
-        findViewById<ImageView>(R.id.retry).setOnClickListener {
-            retry()
-        }
-    }*/
+    private var player: SimpleExoPlayer? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.stream_chat, container, false)
+        return inflater.inflate(R.layout.stream, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val userId = arguments?.getString("userId")
-        val courseId = arguments?.getString("courseId")
-        val course = arguments?.getString("courseName")
-        val firstname = arguments?.getString("firstname")
-        val lastname = arguments?.getString("lastname")
-        val date = arguments?.getString("date")?.replace("T", " ")?.replace(".000Z", "")
-        val desc = arguments?.getString("description")
-        val category = arguments?.getString("category")
-        val level = arguments?.getString("level")
-
-        val stream = Stream()
-        val chat = Chat()
-        val args = Bundle()
-        args.putString("userId", userId)
-        args.putString("courseId", courseId)
-        args.putString("courseName", course)
-        args.putString("firstname",firstname)
-        args.putString("lastname", lastname)
-        args.putString("date", date)
-        args.putString("description", desc)
-        args.putString("category", category)
-        args.putString("level", level)
-        stream.arguments = args
-        chat.arguments = args
-
-        parentFragmentManager
-            .beginTransaction()
-            .replace(R.id.containerVideo, stream)
-            .replace(R.id.containerChat, chat)
-            .commitAllowingStateLoss()
-
-    }
-
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        play(view)
-
-        view.findViewById<ImageView>(R.id.retry).setOnClickListener {
-            retry(view)
-        }
 
         val userId = arguments?.getString("userId")
         val courseId = arguments?.getString("courseId")
@@ -114,15 +57,26 @@ class StreamChat : Fragment(){
                 .beginTransaction()
                 .replace(R.id.container1, fragment)
                 .commitAllowingStateLoss()
+
+            }
+
+        play(view)
+
+        view.findViewById<ImageView>(R.id.retry).setOnClickListener {
+            retry(view)
         }
+
     }
 
     private fun play(view: View) {
-        val STREAMING_URL = "rtmp://193.70.36.58:1935/schoovid/1234"
+        println("TATA")
+        val STREAMING_URL = "rtmp://193.70.36.58:1935/schoovid/94733c29-00f1-4658-a88b-af0ebf681c29"
         player = ExoPlayerFactory.newSimpleInstance(
-            DefaultRenderersFactory(this.context), DefaultTrackSelector(), DefaultLoadControl())
+            DefaultRenderersFactory(this.context), DefaultTrackSelector(), DefaultLoadControl()
+        )
         val playerView = view.findViewById<SimpleExoPlayerView>(R.id.player_view)
         val uri = Uri.parse(STREAMING_URL)
+        println("Toto " + uri)
 
         playerView.player = player
 
@@ -140,6 +94,6 @@ class StreamChat : Fragment(){
     private fun retry(view: View) {
         stop()
         play(view)
-    }*/
+    }
 
 }
